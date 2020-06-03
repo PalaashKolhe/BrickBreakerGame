861f44ba143da26e6d940f74a0f7da4c0c7be2f4
p.kolhe <p.kolhe@share.epsb.ca>
2020-06-03 14:35:30 -0600

    skeleton updated with boxes in level

--- engine.py
+++ engine.py
@@ -7,21 +7,34 @@ title: Engine that glues all objects together
 from window import Window
 from box import Box
 from text import Text
+from loader import *
 import pygame
 
 class Engine:
     def __init__(self):
         self.window = Window()
 
+        # Spawning title box in
+        self.titleBox = Box(self.window)
+        self.titleBox.setDimensions(800, 35)
+        self.titleBox.setPOS(0,0)
+        self.titleBox.color = BLACK
+        self.titleBox.updateSprite()
+
+        # Spawning title in
+        self.title = Text("BRICK BREAKER!", self.window)
+        self.title.setPOS(self.title.window.getWidth() / 2 - 75,
+                          3)
+
         # Spawning player bar in
         self.player = Box(self.window)
-        self.player.setDimensions(78, 16)
+        self.player.setDimensions(100, 20)
         self.player.setPOS(self.player.window.getWidth() / 2 - self.player.width / 2,
                            self.player.window.getHeight() - self.player.height / 2 - 20)
 
         # Spawning ball in
         self.ball = Box(self.window)
-        self.ball.setDimensions(8, 8)
+        self.ball.setDimensions(10, 10)
         self.ball.setPOS(self.ball.window.getWidth() / 2 - self.ball.width / 2,
                          self.ball.window.getHeight() - self.ball.height / 2 - 45)
 
@@ -30,11 +43,28 @@ class Engine:
         self.text.setPOS(self.text.window.getWidth() / 2 - self.text.width / 2 - 43,
                          self.text.window.getHeight() - self.text.height / 2 - 65)
 
-
-
+        self.boxArray = []
 
         # self.title = Text("Brick Breaker", self.window)
         self.running = True
+
+    def levelCreator(self):
+        x = 110 # initial starting point in x plane
+        y = 80 # initial starting point in y plane
+        length = 100
+        height = 40
+
+        rows = 0
+        for j in range(y, y + (height * 6), height + 5):
+            for i in range(x, x + (length * 6), length + 5):
+                box = Box(self.window)
+                box.setDimensions(length, height)
+                if rows % 2 == 0:
+                    box.setPOS(i, j)
+                else:
+                    box.setPOS(i - 50, j)
+                self.boxArray.append(box)
+            rows += 1
 
     def run(self):
         while self.running:
@@ -46,9 +76,18 @@ class Engine:
 
             # OUTPUTS #
             self.window.clearScreen()
+
+            self.window.blitSprite(self.titleBox)
+            self.window.blitSprite(self.title)
+
             self.window.blitSprite(self.player)
             self.window.blitSprite(self.ball)
             self.window.blitSprite(self.text)
+
+            self.levelCreator()
+
+            for i in range(len(self.boxArray)):
+                self.window.blitSprite(self.boxArray[i])
 
             # self.window.blitSprite(self.title)
             self.window.updateScreen()
