ae004f965d952bd655cdaa8abd69fd1cf3846be6
p.kolhe <p.kolhe@share.epsb.ca>
2020-06-04 16:53:19 -0600

    lives feature added

---
+++ life.py
@@ -0,0 +1,22 @@
+# life.py
+
+'''
+title: Life Counter Sprite
+'''
+
+from text import Text
+
+class Lives(Text):
+    def __init__(self, window):
+        Text.__init__(self, "Lives Left: 3", window)
+        self.lives = 3
+
+    # MODIFIER METHODS #
+    def updateLives(self, num):
+        self.lives += num
+        self.setText("Lives Left: " + str(self.lives))
+
+    # ACCESSOR METHODS #
+    def getLives(self):
+        return self.lives
+
